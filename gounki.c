e5227c855635771d348f62ff696246f68a58ad24
Mat <vu.mathieu@gmail.com>
2014-04-28 20:54:00 +0200

    Création de fonctions auxilliaires Révision de l'affichage

--- gounki.c
+++ gounki.c
@@ -1,23 +1,32 @@
 #include "gounki.h"
 
-int main(){
-	int y1, y2, x1, x2, c = 0;
-	char* line;
+void read_options(int argc, char* argv[]){
+}
+
+void read_line(int* x1, int* y1, int* x2, int* y2){
+	char* line = NULL;
 	size_t size;
+
+	getline(&line, &size, stdin);
+
+	*x1 = trans_coord(line[0]);
+	*x2 = trans_coord(line[3]);
+	*y1 = 7 - trans_coord(line[1]);
+	*y2 = 7 - trans_coord(line[4]);
+	
+	free(line);
+}
+
+int main(int argc, char* argv[]){
+	int x1, y1, x2, y2, c = 0;
 	plateau* p = init_plateau();
 	affiche_plateau(p);
 	while(1){
-		printf("Au tour du joueur %s:\n", (c % 2 == 0) ? "violet": "vert");
+		printf("Au tour du %sjoueur %d%s > ", (c % 2 == 0) ? PURPLE : GREEN, (c % 2) + 1, DEFAULT_COLOR);
 		c++;
-		line = NULL;
-		getline(&line, &size, stdin);
-		x1 = trans_coord(line[0]);
-		x2 = trans_coord(line[3]);
-		y1 = 7 - trans_coord(line[1]);
-		y2 = 7 - trans_coord(line[4]);
-		free(line);
+		read_line(&x1, &y1, &x2, &y2);
 		if(!deplacement_possible(p, x1, y1, x2, y2, c)){
-			printf("Erreur, recommencez:\n");
+			printf("Erreur, recommencez\n");
 			c--;
 		}
 		else{
