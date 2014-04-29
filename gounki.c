eb7e169d0d872f8a626d6fa96c3688fab0dd8c0e
Mat <vu.mathieu@gmail.com>
2014-04-29 16:55:32 +0200

    Création de la fonction game_loop, fonction auxilliaire.

--- gounki.c
+++ gounki.c
@@ -1,6 +1,19 @@
 #include "gounki.h"
 
 void read_option(int argc, char* argv[]){
+	int i;
+	/* si il n'y a pas d'option, la fonction return*/
+	if(argc <= 1){
+		return;
+	}
+	else{ 
+		for(i=1 ; i < argc ; i++){
+			/* recherche du mode test */
+			if(!strcmp(argv[i],"-t")){
+				
+			}
+		}
+	}
 }
 
 int read_line(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3, int* x4, int* y4, int* type, int* size_line){
@@ -13,10 +26,11 @@ int read_line(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3, int* x4, int
 	/* retourne 2 si c'est un déploiement */
 	if(line[2] == '+' || line[2] == '*'){
 
+		/* si la commande entrée est trop courte ou trop longue */
 		if(size < 9 || size > 12){
 			return 0;
 		}
-
+		/* transformation des coordonées */
 		*x1 = trans_coord(line[0]);
 		*y1 = 7 - trans_coord(line[1]);
 		
@@ -30,17 +44,19 @@ int read_line(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3, int* x4, int
 			*x4 = trans_coord(line[9]);
 			*y4 = 7 - trans_coord(line[10]);
 		}
+		/* enregistrement du type de déploiement */
 		if(line[2] == '+'){
 			*type = 1;
 		}
 		else{
 			*type = 2;
 		}
+		/* entrée de la longueur de la ligne */
 		*size_line = size;
 		free(line);
 		return 2;
 	}
-	/* retourne 3 si c'est une victoire */
+	/* retourne 3 si une victoire doit être détectée */
 	if(line[2] == '#'){
 		*x1 = trans_coord(line[0]);
 		*y1 = 7 - trans_coord(line[1]);
@@ -59,10 +75,8 @@ int read_line(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3, int* x4, int
 	return 0;
 }
 
-int main(){
+void game_loop(){
 	int  x1, y1, x2, y2, x3, y3, x4, y4, type, size, c = 0, action;
-	plateau* p = init_plateau();
-	affiche_plateau(p);
 	while(1){
 		printf("Au tour du %sjoueur %d%s > ", (c % 2 == 0) ? PURPLE : GREEN, (c % 2) + 1, DEFAULT_COLOR);
 		c++;
@@ -86,7 +100,6 @@ int main(){
 					affiche_plateau(p);
 				}
 				break;
-
 			/* cas 2: déploiement */
 			case 2:
 				if(!deploiement_possible(p,x1, y1, x2, y2, x3, y3, x4, y4, type, c)){
@@ -98,7 +111,19 @@ int main(){
 					affiche_plateau(p);	    
 				}
 				break;
+			/* cas 3: victoire (manque fonction de détection de victoire */
+			case 3:
+				printf("\nFin de partie !\n");
+				return 0;
 		}
 	}
 	return 0;	
+}
+
+int main(int argc, char* argv[]){
+	/* initialisation puis affichage du plateau de jeu */
+	plateau* p = init_plateau();
+	affiche_plateau(p);
+
+	return 0;
 }
